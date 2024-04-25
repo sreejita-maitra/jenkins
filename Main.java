e573c53415b65baa9d0dc38e8b73f0bc711d0146
Sreejita Maitra <sreejita.maitra@gmail.com>
2024-04-25 09:14:33 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Jenkins;
+
+public class Main {
+	
+	public static void main(String[] args){
+		System.out.println("Hello! I'm learning Jenkins,"
+				+ "\n Here is my Assignment 2 for Jenkins");
+	}
+}
+
